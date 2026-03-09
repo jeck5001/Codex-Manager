29be0595cfcfa2c37dbb93544c5db91298e6612c
高 宏顺 <18272669457@163.com>
2026-03-09 15:49:03 +0000

    整理桌面端命令注册模块

---
+++ apps/src-tauri/src/commands/registry.rs
@@ -0,0 +1,78 @@
+macro_rules! invoke_handler {
+    () => {
+        tauri::generate_handler![
+            // service
+            crate::commands::service::service_start,
+            crate::commands::service::service_stop,
+            crate::commands::service::service_initialize,
+            crate::commands::service::service_rpc_token,
+
+            // account
+            crate::commands::account::remote::service_account_list,
+            crate::commands::account::remote::service_account_delete,
+            crate::commands::account::remote::service_account_delete_many,
+            crate::commands::account::remote::service_account_delete_unavailable_free,
+            crate::commands::account::remote::service_account_update,
+            crate::commands::account::transfer::service_account_import,
+            crate::commands::account::transfer::service_account_import_by_directory,
+            crate::commands::account::transfer::service_account_export_by_account_files,
+            crate::commands::account::local::local_account_delete,
+
+            // usage
+            crate::commands::usage::service_usage_read,
+            crate::commands::usage::service_usage_list,
+            crate::commands::usage::service_usage_refresh,
+
+            // request log
+            crate::commands::requestlog::service_requestlog_list,
+            crate::commands::requestlog::service_requestlog_clear,
+            crate::commands::requestlog::service_requestlog_today_summary,
+
+            // settings
+            crate::commands::settings::service_listen::service_listen_config_get,
+            crate::commands::settings::service_listen::service_listen_config_set,
+            crate::commands::settings::gateway::service_gateway_route_strategy_get,
+            crate::commands::settings::gateway::service_gateway_route_strategy_set,
+            crate::commands::settings::gateway::service_gateway_manual_account_get,
+            crate::commands::settings::gateway::service_gateway_manual_account_set,
+            crate::commands::settings::gateway::service_gateway_manual_account_clear,
+            crate::commands::settings::gateway::service_gateway_header_policy_get,
+            crate::commands::settings::gateway::service_gateway_header_policy_set,
+            crate::commands::settings::gateway::service_gateway_background_tasks_get,
+            crate::commands::settings::gateway::service_gateway_background_tasks_set,
+            crate::commands::settings::gateway::service_gateway_upstream_proxy_get,
+            crate::commands::settings::gateway::service_gateway_upstream_proxy_set,
+            crate::commands::settings::ui::app_settings_get,
+            crate::commands::settings::ui::app_settings_set,
+            crate::commands::settings::ui::app_close_to_tray_on_close_get,
+            crate::commands::settings::ui::app_close_to_tray_on_close_set,
+
+            // login
+            crate::commands::login::service_login_start,
+            crate::commands::login::service_login_status,
+            crate::commands::login::service_login_complete,
+
+            // api key
+            crate::commands::apikey::service_apikey_list,
+            crate::commands::apikey::service_apikey_read_secret,
+            crate::commands::apikey::service_apikey_create,
+            crate::commands::apikey::service_apikey_models,
+            crate::commands::apikey::service_apikey_update_model,
+            crate::commands::apikey::service_apikey_delete,
+            crate::commands::apikey::service_apikey_disable,
+            crate::commands::apikey::service_apikey_enable,
+
+            // system
+            crate::commands::system::open_in_browser,
+
+            // updater
+            crate::commands::updater::app_update_check,
+            crate::commands::updater::app_update_prepare,
+            crate::commands::updater::app_update_apply_portable,
+            crate::commands::updater::app_update_launch_installer,
+            crate::commands::updater::app_update_status
+        ]
+    };
+}
+
+pub(crate) use invoke_handler;
